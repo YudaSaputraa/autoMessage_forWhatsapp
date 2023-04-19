b11e70ba4dc09cbf89c7b36592b1387843bee8c1
Yudasaputraa <yudasaputra082@gmail.com>
2023-04-19 13:14:49 +0700

    edited

--- autoMessage.py
+++ autoMessage.py
@@ -2,11 +2,11 @@ import pywhatkit
 import pyautogui
 import time
 
-phoneNumber = "+6285158911402"
+phoneNumber = ["+6287861009087", "+6285158911402"]
 message = "Presensi duluu [automatic Message with python]"
 hour = 12
-minute = 00
-pywhatkit.sendwhatmsg(phoneNumber, message,hour,minute)
+minute = 29
+pywhatkit.sendwhatmsg(phoneNumber[1], message, hour, minute)
 time.sleep(1)
 pyautogui.click()
 time.sleep(1)
